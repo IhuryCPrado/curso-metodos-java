74c2884665c422107cee455965477fc35014c54e
IhuryCPrado <ihurycprado@gmail.com>
2022-10-20 21:31:20 -0300

    Exercício de Sobrecarga

---
+++ src/MainSobrecarga.java
@@ -0,0 +1,11 @@
+public class MainSobrecarga {
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(17);
+        Quadrilatero.area(5d,8d);
+        Quadrilatero.area(7,55,2);
+        Quadrilatero.area(7f,8f);
+    }
+}
