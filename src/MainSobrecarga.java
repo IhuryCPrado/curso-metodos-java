b82ff34aa63ded51974f2ea72d631176aae2b3a8
IhuryCPrado <ihurycprado@gmail.com>
2022-10-20 22:08:05 -0300

    Exercício de Sobrecarga

--- src/MainSobrecarga.java
+++ src/MainSobrecarga.java
@@ -1,11 +1,19 @@
 public class MainSobrecarga {
     public static void main(String[] args) {
 
-        // Quadrilátero
-        System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(17);
-        Quadrilatero.area(5d,8d);
-        Quadrilatero.area(7,55,2);
-        Quadrilatero.area(7f,8f);
+        // Retornos
+        System.out.println("Exercício Retornos");
+
+        double areaQuadrado = Quadrilatero.area(17);
+        System.out.println("Área do Quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(17,6);
+        System.out.println("Área do Retângulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(4,8,12);
+        System.out.println("Área do Trapézio: " + areaTrapezio);
+
+        double areaLosango = Quadrilatero.area(17f,3f);
+        System.out.println("Área do Losango: " + areaLosango);
     }
 }
